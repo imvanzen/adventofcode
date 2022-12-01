b7173ee22264284d0eb9ef2704c9e7f3e847fa98
J.Reczko <reczko.jakub@gmail.com>
2022-12-01 14:19:01 +0100

    Cleanup builds add gitignore

--- 2022/1/a.ts
+++ 2022/1/a.ts
@@ -1,30 +1,29 @@
 #!node
 
-import * as fs from 'fs'
-import * as path from 'path'
+import * as fs from 'fs';
+import * as path from 'path';
 
 function readFile() {
-    const fileContents = fs.readFileSync(
-        path.join(__dirname, './a.input.txt'), {
-            encoding: 'utf-8',
-        },
-    );
-
-    let curr: number = 0;
-    let maxIndex: number = 0;
-    let aggr: number[] = [0];
-    const allCalories: string[] = fileContents.split('\r\n')
-    
-    for (let index in allCalories) {
-        if (allCalories[index] === '') {
-            curr++
-            aggr[curr] = 0
-        } else {
-            aggr[curr] += parseInt(allCalories[index])
-        }
+  const fileContents = fs.readFileSync(
+    path.join(__dirname, './a.input.txt'), {
+      encoding: 'utf-8',
     }
+  );
 
-    console.log("elf with the max number of calories", Math.max(...aggr))
+  let curr: number = 0;
+  let aggr: number[] = [0];
+  const allCalories: string[] = fileContents.split('\r\n');
+  
+  for (let index in allCalories) {
+    if (allCalories[index] === '') {
+      curr++
+      aggr[curr] = 0;
+    } else {
+      aggr[curr] += parseInt(allCalories[index]);
+    }
+  }
+
+  console.log("elf with the max number of calories", Math.max(...aggr))
 }
 
 readFile()
--- 2022/1/b.ts
+++ 2022/1/b.ts
@@ -4,30 +4,30 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 function readFile() {
-    const fileContents = fs.readFileSync(
-        path.join(__dirname, './a.input.txt'), {
-            encoding: 'utf-8',
-        },
-    );
+  const fileContents = fs.readFileSync(
+    path.join(__dirname, './a.input.txt'), {
+      encoding: 'utf-8',
+    },
+  );
 
-    let curr: number = 0;
-    let maxIndex: number = 0;
-    let aggr: number[] = [0];
-    const allCalories: string[] = fileContents.split('\r\n')
-    
-    for (let index in allCalories) {
-        if (allCalories[index] === '') {
-            curr++
-            aggr[curr] = 0
-        } else {
-            aggr[curr] += parseInt(allCalories[index])
-        }
+  let curr: number = 0;
+  let maxIndex: number = 0;
+  let aggr: number[] = [0];
+  const allCalories: string[] = fileContents.split('\r\n')
+  
+  for (let index in allCalories) {
+    if (allCalories[index] === '') {
+      curr++
+      aggr[curr] = 0
+    } else {
+      aggr[curr] += parseInt(allCalories[index])
     }
+  }
 
-    const topThree = aggr.sort((a, b) => b - a).splice(0, 3)
+  const topThree = aggr.sort((a, b) => b - a).splice(0, 3)
 
-    console.log("Top three elfes", topThree)
-    console.log("Their sum", topThree.reduce((sum, numb) => sum += numb, 0))
+  console.log("Top three elfes", topThree)
+  console.log("Their sum", topThree.reduce((sum, numb) => sum += numb, 0))
 }
 
 readFile()
