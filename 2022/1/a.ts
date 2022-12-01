803e6c6fc2248aed57c745031f48eabd1261eae6
J.Reczko <reczko.jakub@gmail.com>
2022-12-01 13:21:36 +0100

    Day 1 Part 1

---
+++ 2022/1/a.ts
@@ -0,0 +1,31 @@
+#!node
+
+import * as fs from 'fs'
+import * as path from 'path'
+
+function readFile() {
+    const fileContents = fs.readFileSync(
+        path.join(__dirname, './a.input.txt'), {
+            encoding: 'utf-8',
+        },
+    );
+
+    let curr: number = 0;
+    let maxIndex: number = 0;
+    let aggr: number[] = [0];
+    const allCalories: string[] = fileContents.split('\r\n')
+    
+    for (let index in allCalories) {
+        if (allCalories[index] === '') {
+            curr++
+            aggr[curr] = 0
+        } else {
+            aggr[curr] += parseInt(allCalories[index])
+        }
+    }
+
+    console.log("elf with the max number of calories", Math.max(...aggr))
+
+}
+
+readFile()
